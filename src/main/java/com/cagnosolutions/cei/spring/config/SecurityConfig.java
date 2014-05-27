55bee4402a2052e770246d02dff34721fd78e9e4
gregpechiro <gregpechiro@yahoo.com>
2014-05-27 17:27:16 -0400

    added html template and database auth

--- src/main/java/com/cagnosolutions/cei/spring/config/SecurityConfig.java
+++ src/main/java/com/cagnosolutions/cei/spring/config/SecurityConfig.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebMvcConfigurerAdapter {
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	DataSource dataSource;
@@ -26,16 +26,21 @@ public class SecurityConfig extends WebMvcConfigurerAdapter {
 	}	
 
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-		.and()
-		.formLogin().loginPage("/login").failureUrl("/login?error")
-		.usernameParameter("username").passwordParameter("password")
-		.and()
-		.logout().logoutSuccessUrl("/login?logout")
-		.and()
-		.exceptionHandling().accessDeniedPage("/403")
-		.and()
-		.csrf();
+		http
+			.authorizeRequests()
+				.antMatchers("/admin**").access("hasRole('ROLE_ADMIN')")
+				.antMatchers("/static/**", "/index").permitAll() .anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/auth")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutSuccessUrl("/index?logout")
+				.permitAll()
+				.and()
+			.exceptionHandling().accessDeniedPage("/403")
+				.and()
+			.csrf();
 	}
 }
--- src/main/java/com/cagnosolutions/cei/spring/controller/SiteController.java
+++ src/main/java/com/cagnosolutions/cei/spring/controller/SiteController.java
@@ -1,34 +1,38 @@
 package com.cagnosolutions.cei.spring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.cagnosolutions.cei.spring.domain.User;
-import com.cagnosolutions.cei.spring.service.UserService;
 
 @Controller
 public class SiteController {
-
-	@Autowired
-	private UserService userService;
 	
-	@RequestMapping("/")
-	public String indexHandler(@RequestParam(value="msg", required = false) String msg, Model model) {
-		model.addAttribute("msg", msg);
-		model.addAttribute("user", new User());
-		model.addAttribute("userList", userService.findAll());
+	@RequestMapping("/index")
+	public String indexHandler() {
 		return "index";
 	}
-
-	@RequestMapping("/addUser")
-	public String addUser(@RequestParam(value="confirm", required = true)String confirm, User user, Model model) {
-		if (confirm.equals(user.getPass())) {
-			userService.insert(user);
-			return "redirect:/?msg="+user.getName()+" added successfully.";
-		} else {
-			return "redirect:/?msg=ERROR";
+	
+	@RequestMapping("/home")
+	public String welcomeHandler() {
+		return "home";
+	}
+	
+	@RequestMapping("/admin")
+	public String adminHandler() {
+		return "admin/admin";
+	}
+	
+	@RequestMapping("/error.html")
+	public String errorHandler(HttpServletRequest request, Model model) {
+		model.addAttribute("code", request.getAttribute("javax.servlet.error.status_code"));
+		Throwable throwable = (Throwable)request.getAttribute("javax.servlet.error.exception");
+		String message = null;
+		if (throwable != null) {
+			message = throwable.getMessage();
 		}
+		model.addAttribute("message", message.toString());
+		return "error.html";
 	}
 }
